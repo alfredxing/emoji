111d7e055039d570478f475d7d2cc917f47f74f7
Alfred Xing <alfredxing@users.noreply.github.com>
2018-07-22 18:11:45 -0700

    Whitespace: 2 spaces for Ruby

--- ttf/tables/sbix.rb
+++ ttf/tables/sbix.rb
@@ -1,100 +1,100 @@
 module Emoji
-	module TTF
-		module Tables
-			# `sbix` table
-			# Contains image bitmaps (emoji image data)
-			class SBIX
-				def initialize(raw, numGlyphs)
-					@bytes = raw
-					@numGlyphs = numGlyphs
+  module TTF
+    module Tables
+      # `sbix` table
+      # Contains image bitmaps (emoji image data)
+      class SBIX
+        def initialize(raw, numGlyphs)
+          @bytes = raw
+          @numGlyphs = numGlyphs
 
-					header()
-					strikes()
-				end
+          header()
+          strikes()
+        end
 
-				# Table header
-				#
-				# Offset    Type        Name
-				# 0         UInt16      version
-				# 2         UInt16      flags (unused)
-				# 4         UInt32      numStrikes
-				# 8         UInt32[]    strikeOffset[numStrikes]
-				def header
-					@version, @flags, @numStrikes = @bytes[0, 8].unpack('nnN')
+        # Table header
+        #
+        # Offset    Type        Name
+        # 0         UInt16      version
+        # 2         UInt16      flags (unused)
+        # 4         UInt32      numStrikes
+        # 8         UInt32[]    strikeOffset[numStrikes]
+        def header
+          @version, @flags, @numStrikes = @bytes[0, 8].unpack('nnN')
 
-					@strikeOffset = [*0...@numStrikes].map do |n|
-						start = 8 + n * 4
-						@bytes[start, 4].unpack('N')[0]
-					end
-				end
+          @strikeOffset = [*0...@numStrikes].map do |n|
+            start = 8 + n * 4
+            @bytes[start, 4].unpack('N')[0]
+          end
+        end
 
-				# Strike data records
-				#
-				# Offset    Type        Name
-				# 0         UInt16      ppem
-				# 2         UInt16      resolution (unused)
-				# 4         UInt32[]    glyphDataOffset[numGlyphs + 1]
-				def strikes
-					if @strikes then
-						return @strikes
-					end
+        # Strike data records
+        #
+        # Offset    Type        Name
+        # 0         UInt16      ppem
+        # 2         UInt16      resolution (unused)
+        # 4         UInt32[]    glyphDataOffset[numGlyphs + 1]
+        def strikes
+          if @strikes then
+            return @strikes
+          end
 
-					@strikes = @strikeOffset.map do |offset|
-						ppem, resolution = @bytes[offset, 4].unpack('nn')
-						glyphDataOffset = [*0...(@numGlyphs + 1)].map do |n|
-							@bytes[offset + 4 + (n * 4), 4].unpack('N')[0]
-						end
+          @strikes = @strikeOffset.map do |offset|
+            ppem, resolution = @bytes[offset, 4].unpack('nn')
+            glyphDataOffset = [*0...(@numGlyphs + 1)].map do |n|
+              @bytes[offset + 4 + (n * 4), 4].unpack('N')[0]
+            end
 
-						glyphs = [*0...@numGlyphs].map do |glyphID|
-							start = glyphDataOffset[glyphID]
-							length = glyphDataOffset[glyphID + 1] - start
+            glyphs = [*0...@numGlyphs].map do |glyphID|
+              start = glyphDataOffset[glyphID]
+              length = glyphDataOffset[glyphID + 1] - start
 
-							if length == 0 then
-								nil
-							else
-								Glyph.new(@bytes[offset + start, length])
-							end
-						end
+              if length == 0 then
+                nil
+              else
+                Glyph.new(@bytes[offset + start, length])
+              end
+            end
 
-						Strike.new({
-							:ppem => ppem,
-							:resolution => resolution,
-							:glyphDataOffset => glyphDataOffset,
-							:glyphs => glyphs
-						})
-					end
-				end
-			end
+            Strike.new({
+              :ppem => ppem,
+              :resolution => resolution,
+              :glyphDataOffset => glyphDataOffset,
+              :glyphs => glyphs
+            })
+          end
+        end
+      end
 
-			class SBIX::Strike
-				attr_reader :ppem, :resolution, :glyphDataOffset, :glyphs
+      class SBIX::Strike
+        attr_reader :ppem, :resolution, :glyphDataOffset, :glyphs
 
-				def initialize(ppem:, resolution: nil, glyphDataOffset: nil, glyphs:)
-					@ppem = ppem
-					@resolution = resolution
-					@glyphDataOffset = glyphDataOffset
-					@glyphs = glyphs
-				end
-			end
+        def initialize(ppem:, resolution: nil, glyphDataOffset: nil, glyphs:)
+          @ppem = ppem
+          @resolution = resolution
+          @glyphDataOffset = glyphDataOffset
+          @glyphs = glyphs
+        end
+      end
 
-			# Glyph bitmap
-			#
-			# Offset    Type        Name
-			# 0         SInt16      originOffsetX
-			# 2         SInt16      originOffsetY
-			# 4         Tag         graphicType
-			# 8         UInt8[]     data[]
-			class SBIX::Glyph
-				attr_reader :originOffsetX, :originOffsetY, :graphicType, :data
+      # Glyph bitmap
+      #
+      # Offset    Type        Name
+      # 0         SInt16      originOffsetX
+      # 2         SInt16      originOffsetY
+      # 4         Tag         graphicType
+      # 8         UInt8[]     data[]
+      class SBIX::Glyph
+        attr_reader :originOffsetX, :originOffsetY, :graphicType, :data
 
-				def initialize(bytes)
-					@bytes = bytes
+        def initialize(bytes)
+          @bytes = bytes
 
-					@originOffsetX, @originOffsetY = @bytes[0, 4].unpack('s>s>')
-					@graphicType = @bytes[4, 4]
-					@data = @bytes[8, @bytes.length - 8]
-				end
-			end
-		end
-	end
+          @originOffsetX, @originOffsetY = @bytes[0, 4].unpack('s>s>')
+          @graphicType = @bytes[4, 4]
+          @data = @bytes[8, @bytes.length - 8]
+        end
+      end
+    end
+  end
 end
--- ttf/ttc.rb
+++ ttf/ttc.rb
@@ -1,39 +1,39 @@
 module Emoji
-	module TTF
-		# Processes the TTC (TrueType Container) font container.
-		# A TTC file is a simple header followed by the TTF fonts it contains.
-		#
-		# Offset    Type        Name
-		# 0         TAG         ttcTag
-		# 4         UInt16      majorVersion
-		# 6         UInt16      minorVersion
-		# 8         UInt32      numFonts
-		# 12        UInt32[]    offsetTable[numFonts]
-		class TTC
-			attr_reader :fonts
+  module TTF
+    # Processes the TTC (TrueType Container) font container.
+    # A TTC file is a simple header followed by the TTF fonts it contains.
+    #
+    # Offset    Type        Name
+    # 0         TAG         ttcTag
+    # 4         UInt16      majorVersion
+    # 6         UInt16      minorVersion
+    # 8         UInt32      numFonts
+    # 12        UInt32[]    offsetTable[numFonts]
+    class TTC
+      attr_reader :fonts
 
-			def initialize(bytes)
-				@bytes = bytes
+      def initialize(bytes)
+        @bytes = bytes
 
-				validate()
-				process()
-			end
+        validate()
+        process()
+      end
 
-			def validate()
-				if @bytes[0, 4] != 'ttcf' then
-					raise 'Invalid TTC'
-				end
-			end
+      def validate()
+        if @bytes[0, 4] != 'ttcf' then
+          raise 'Invalid TTC'
+        end
+      end
 
-			def process()
-				# TTC header
-				majorVersion, minorVersion, numFonts = @bytes[4, 8].unpack('nnN')
-				offsetTable = @bytes[12, (4 * numFonts)].unpack('N' * numFonts)
+      def process()
+        # TTC header
+        majorVersion, minorVersion, numFonts = @bytes[4, 8].unpack('nnN')
+        offsetTable = @bytes[12, (4 * numFonts)].unpack('N' * numFonts)
 
-				@fonts = offsetTable.map do |offset|
-					TTF.new(@bytes[offset, @bytes.length], offset)
-				end
-			end
-		end
-	end
+        @fonts = offsetTable.map do |offset|
+          TTF.new(@bytes[offset, @bytes.length], offset)
+        end
+      end
+    end
+  end
 end
